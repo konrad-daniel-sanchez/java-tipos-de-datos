85f1fd76a97ab408096a345d70f3675d740451c5
DANIELA <laurad.galindoc@konradlorenz.edu.co>
2023-08-31 22:29:52 -0500

    Se agrego el quiz2 del cifrador cesar y el decodificador.

---
+++ src/Decodificador_Cesar.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class Decodificador_Cesar {
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        System.out.println("Ingresa el mensaje cifrado");
+        String mensajeCifrado = lector.next();
+        System.out.println("Número de veces que se movió la letra");
+        int veces = lector.nextInt();
+
+        String mensajeOriginal = "";
+
+        for(int i = 0; i < mensajeCifrado.length(); i++) {
+            char letra = mensajeCifrado.charAt(i);
+            boolean minuscula = (letra >= 'a' && letra <='z');
+            boolean mayuscula = (letra >= 'A' && letra <='Z');
+            if(!(minuscula || mayuscula))
+                mensajeOriginal += letra;
+            else {
+                int ascii = (int)letra;
+                int baseAscii = (int)'a';
+                if(mayuscula)
+                    baseAscii = (int)'A';
+                int nuevoAscii = (ascii - baseAscii - veces) % 26 + baseAscii;
+                char nuevaLetra = (char)nuevoAscii;
+                mensajeOriginal += nuevaLetra;
+            }
+        }
+        System.out.println("Mensaje original: " + mensajeOriginal);
+
+    }
+}
